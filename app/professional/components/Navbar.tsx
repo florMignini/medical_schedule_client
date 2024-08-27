fc1554036e1bbe13fd1521cdbb9de769b04e7a8a
florMignini <flormignini29@gmail.com>
2024-08-27 08:45:59 -0300

    professional navbar

---
+++ app/professional/components/Navbar.tsx
@@ -0,0 +1,16 @@
+
+const Navbar = () => {
+  return (
+    <nav className='w-full h-14 flex items-center justify-between'>
+        <div className='flex flex-col w-[50%] text-white opacity-65'>
+            <h2 className='text-18-bold'>Bienvenido</h2>
+            <p className='text-12-regular opacity-20'>Hola, Leandro bienvenido de vuelta.</p>
+        </div>
+        <div>
+            {/* UserImage */}
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
