78a6146391f77c949df47f7d3987824cd20e9ae0
florMignini <flormignini29@gmail.com>
2024-08-31 07:16:25 -0300

    sidebar responsive design update

--- components/ProfessionalSidebar.tsx
+++ components/ProfessionalSidebar.tsx
@@ -1,23 +1,50 @@
 import Icon from "../components/ui/icon";
 import Link from "next/link";
+import Image from "next/image";
+import Logo from "../public/assets/medical_schedule-logo.svg";
 import { ProfessionalSidebarData } from "../data/ProfessionalSidebarData";
+import CloseIcon from "../public/assets/icons/close.svg";
 import { toggleSideI } from "@/interfaces";
 
-const ProfessionalSidebar = ({ isOpen }: toggleSideI) => {
+const ProfessionalSidebar = ({ isOpen, toggleSidebar }: toggleSideI) => {
   return (
     <aside
       className={` ${
         isOpen
-          ? "fixed inset-y-0 left-0 w-64 bg-gray-800 text-white transform translate-x-0"
+          ? "fixed inset-y-0 left-0 w-64 md:w-[50%] backdrop-blur-lg text-white transform translate-x-0"
           : "hidden lg:flex sidebar items-center justify-start flex-col -translate-x-full"
-      } transition-transform duration-300 ease-in-out lg:translate-x-0`}
+      } transition-transform duration-500 ease-in-out lg:translate-x-0`}
     >
-      <div className="w-full flex flex-col items-center justify-center mx-auto gap-2">
+      {isOpen && (
+        <div className="w-full pt-2 px-5 flex items-center justify-between">
+          <div className="flex flex-col items-center justify-center">
+            <Image
+              src={Logo}
+              alt="medical-schedule-logo"
+              height={40}
+              width={40}
+            />
+            <p className="text-[10px]">medical schedule</p>
+          </div>
+          <button className="flex" onClick={toggleSidebar}>
+            <Icon src={CloseIcon} alt="close-icon" width={30} height={30} />
+          </button>
+        </div>
+      )}
+      <div
+        className={`w-full flex flex-col items-center justify-center mx-auto gap-2 ${
+          isOpen ? "py-16" : ""
+        }`}
+      >
         {ProfessionalSidebarData.map((item, index) => (
           <Link
             href={item.path}
             key={index}
-            className="h-12 w-full flex gap-2 items-center text-light-200 opacity-65 hover:bg-dark-500 rounded-lg"
+            className={`w-[85%] flex gap-2 items-center text-light-200 h-12 ${
+              isOpen
+                ? "justify-center hover:scale-110 hover:font-extrabold"
+                : "opacity-65 hover:bg-dark-500 rounded-lg pl-2"
+            }`}
           >
             <Icon
               src={item.icon.src}
