7309442faa05a86f759391697a7276ff5feafa28
florMignini <flormignini29@gmail.com>
2024-09-07 09:19:52 -0300

    package & const update

--- data/allergiesOptions.ts
+++ data/allergiesOptions.ts
@@ -1,1 +1,20 @@
 export const AllergiesType = ["Alimentos", "Drogas/Medicamentos","Ambientales/Estacionales", "Contacto", "Otras"]
+
+export const AllergiesDescription = [
+    {
+        type: "Alimentos",
+        content: ["Mani", "Mariscos", "Pescado", "Lácteos", "Huevo", "Trigo/Gluten", "Soja", "Otro"]
+    },
+    {
+        type: "Drogas/Medicamentos",
+        content: ["Penicilina", "Drogas Sulfa", "NSAIDs", "Cefalosporinas", "Anestésicos", "Insulina", "Otro"]
+    },
+    {
+        type: "Ambientales/Estacionales",
+        content: ["Pollen", "Ácaros del polvo", "Espóras de moho", "Caspa de animales", "Picaduras de insectos", "Látex", "Otro"]
+    },
+    {
+        type: "Contacto",
+        content: ["Metales/Joyas", "Fragancias", "Detergentes/Jabones", "Caspa de animales", "Formaldehído", "Otro"]
+    },
+]
