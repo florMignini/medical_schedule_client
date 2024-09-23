d996fad2b3df22b802a50fcb0d514845b03abc4e
florMignini <flormignini29@gmail.com>
2024-09-23 19:01:24 -0300

    update dashboard dates

--- app/professional/components/WelcomeSection.tsx
+++ app/professional/components/WelcomeSection.tsx
@@ -1,49 +1,67 @@
-"use client"
-import Image from 'next/image'
+"use client";
+import Image from "next/image";
 import WelcomeGif from "../../../public/assets/medical-welcome.gif";
-import { useEffect, useState } from 'react';
-import { getDate } from '@/utils/getDate';
+import Icon from "@/components/ui/icon";
+import { useEffect, useState } from "react";
+import { getDate, getDay } from "@/utils/getDate";
+import CalendarIcon from "../../../public/assets/icons/calendar.svg"
 
 const WelcomeSection = () => {
-const [todayDate, setTodayDate] = useState<string>()
-const [infoProfSession, setInfoProfSession] = useState<any>();
-    useEffect(() => {
-      let date = getDate()
-      setTodayDate(date)
-    }, [])
-     useEffect(() => {
-       let profData = localStorage.getItem('infoProfSession');
-       if (profData) {
-         setInfoProfSession(JSON.parse(profData));
-       }
-     }, []);
-    console.log(infoProfSession)
+  const [todayDay, setTodayDay] = useState<string>();
+  const [infoProfSession, setInfoProfSession] = useState<any>();
+  const [todayDate, setTodayDate] = useState<string>();
+  // effect for week day
+  useEffect(() => {
+    let date = getDay();
+    setTodayDay(date);
+  }, []);
+  // effect for prof session info
+  useEffect(() => {
+    let profData = localStorage.getItem("infoProfSession");
+    if (profData) {
+      setInfoProfSession(JSON.parse(profData));
+    }
+  }, []);
+//   effect for todays full date
+useEffect(() => {
+  let getTodayDate = getDate()
+  setTodayDate(getTodayDate);
+}, [])
+
   return (
-    <div className="w-[99%] grid grid-cols-[60%,40%] py-2 px-5 bg-dark-400 rounded-md">
-    {/* Welcome */}
-    <div className="w-full flex flex-col items-start pl-1 justify-between gap-1">
-      {/* date */}
-        <div className="w-36 h-7 px-2 flex items-center rounded-md bg-dark-500">
-              <h1>Today Date</h1>
+    <div className="w-[99%] grid grid-cols-[60%,40%] py-2 px-5 bg-dark-400 rounded-md ">
+      {/* Welcome */}
+      <div className="w-full flex flex-col items-start pl-1 justify-evenly gap-1">
+        {/* date */}
+        <div className="w-36 h-7 px-2 flex justify-around items-center rounded-md bg-dark-500 text">
+            <Icon
+            src={CalendarIcon}
+            alt="calendar-icon"
+            width={15}
+            height={15}
+            />
+          <h1 className="font-light ">{todayDate}</h1>
         </div>
         {/* Message */}
         <div className="w-[90%] flex flex-col gap-2 pb-1">
-            <h1 className="text-24-bold capitalize">{`Bienvenido Dr. ${infoProfSession.lastname}`}</h1>
-            <p className="text-12-regular text-light-200 capitalize">{`que tengas un lindo ${todayDate ? todayDate : `...`}`}</p>
+          <h1 className="text-24-bold capitalize">{`Bienvenido Dr. ${infoProfSession ? infoProfSession.lastname : "..."}`}</h1>
+          <p className="text-base font-light text-light-200 capitalize">{`que tengas un lindo ${
+            todayDay ? todayDay : `...`
+          }`}</p>
         </div>
+      </div>
+      {/* gif */}
+      <div className="w-full flex items-center justify-end">
+        <Image
+          src={WelcomeGif}
+          alt="welcome-gif"
+          width={200}
+          height={200}
+          className="rounded-md"
+        />
+      </div>
     </div>
-    {/* gif */}
-    <div className="w-full flex items-center justify-end">
-      <Image
-        src={WelcomeGif}
-        alt="welcome-gif"
-        width={200}
-        height={200}
-        className="rounded-md"
-      />
-    </div>
-  </div>
-  )
-}
+  );
+};
 
-export default WelcomeSection
+export default WelcomeSection;
--- utils/getDate.ts
+++ utils/getDate.ts
@@ -1,8 +1,14 @@
-export  function getDate() {
+export  function getDay() {
   const dayOfWeek = ["Domingo", "Lunes", "Martes", "Miércoles", "Sábado"]
     const today = new Date();
     const day = today.getDay();
-    const year = today.getFullYear();
-    const date = today.getDate();
     return dayOfWeek[day];
   }
+
+export function getDate(){
+  const today = new Date();
+  const month = today.getMonth() + 1;
+  const year = today.getFullYear();
+  const date = today.getDate();
+  return `${date}/${month}/${year}`;
+}
