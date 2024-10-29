880fd5ea306ea1957d522a0f8fddd4c1b80dbb9c
florMignini <flormignini29@gmail.com>
2024-10-29 07:38:04 -0300

    logo ticker ui

---
+++ app/introducing-medical-schedule/components/LogoTicker.tsx
@@ -0,0 +1,27 @@
+import medicalScheduleLogo from "@/public/assets/medical_schedule-logo.svg"
+import Image from "next/image";
+
+const LogoTicker = () => {
+    return <section className="max-[490px]:py-0 max-[768px]:py-10  sm:py-12 bg-dark-300/30">
+        <div className="container">
+            <div className="flex overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)]">
+                <div className="flex flex-none gap-14 ">
+                    {
+                        [medicalScheduleLogo, medicalScheduleLogo, medicalScheduleLogo, medicalScheduleLogo,
+                            medicalScheduleLogo, medicalScheduleLogo].map((logo) => (
+                            <Image
+                                src={logo.src}
+                                key={logo.src}
+                                alt="logo-image"
+                                height={150}
+                                width={150}
+                            />
+                        ))
+                    }
+                </div>
+            </div>
+        </div>
+    </section>
+}
+
+export default LogoTicker;
--- app/introducing-medical-schedule/page.tsx
+++ app/introducing-medical-schedule/page.tsx
@@ -1,14 +1,13 @@
-import Image from "next/image";
-import Logo from "../../public/assets/medical_schedule-logo.svg";
-import Link from "next/link";
 import Header from "@/app/introducing-medical-schedule/components/Header";
 import Hero from "@/app/introducing-medical-schedule/components/Hero";
+import LogoTicker from "@/app/introducing-medical-schedule/components/LogoTicker";
 
 const LandingPage = () => {
     return (
         <>
             <Header/>
             <Hero/>
+            <LogoTicker/>
         </>
     );
 };
