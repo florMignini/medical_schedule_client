b797014c6bc0ec63f6dda1aa8d384506154e133a
florMignini <flormignini29@gmail.com>
2024-11-26 18:42:40 -0300

    update global styles & productImage

--- app/introducing-medical-schedule/components/Hero.tsx
+++ app/introducing-medical-schedule/components/Hero.tsx
@@ -11,7 +11,7 @@ const Hero = () => {
             opacity:"80%"
         }}>
             <div className="relative max-[490px]:w-[95%] max-[768px]:w-[90%] h-full sm:w-[70%] flex flex-col items-start justify-center">
-                <h1 className="absolute h-auto py-2 px-1 top-8 sm:top-12 max-[490px]:text-[20px] max-[768px]:text-[30px] max-[490px]:ml-0 max-[768px]:ml-7 sm:text-5xl sm:mx-auto font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent text-center sm:text-end bg-clip-text sm:leading-normal">Medical Schedule - Proveemos el mejor sistema de administración de pacientes</h1>
+                <h1 className="absolute h-auto title">Medical Schedule - Proveemos el mejor sistema de administración de pacientes</h1>
                 <p className="text-center font-light max-[490px]:top-[10rem] max-[768px]:top-[12rem] sm:top-[15rem] lg:top-[18rem] h-auto py-2 absolute max-[490px]:text-[14px] max-[768px]:text-[16px] sm:text-xl text-[#010D3E] tracking-tight left-[30%]">
                     Seguí tus pacientes y actividades facilmente desde donde estés
                 </p>
