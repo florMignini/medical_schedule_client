fb8c49949cb5c65f248584df8369899df82865a3
florMignini <flormignini29@gmail.com>
2024-11-30 06:56:46 -0300

    logo ticker animation

--- app/introducing-medical-schedule/components/LogoTicker.tsx
+++ app/introducing-medical-schedule/components/LogoTicker.tsx
@@ -1,12 +1,24 @@
+"use client"
 import medicalScheduleLogo from "@/public/assets/medical_schedule-logo.svg";
 import Image from "next/image";
+import {motion} from "framer-motion"
 
 const LogoTicker = () => {
   return (
     <section className="max-[490px]:py-5 max-[768px]:py-10  sm:py-12 bg-dark-300/30">
       <div className="container">
         <div className="flex overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)]">
-          <div className="flex flex-none gap-14 ">
+          <motion.div className="flex flex-none gap-14 "
+          animate={{
+            translateX: "-50%",
+          }}
+          transition={{
+            duration: 20,
+            ease: "linear",
+            repeat: Infinity,
+            repeatType: "loop",
+          }}
+          >
             {[
               medicalScheduleLogo,
               medicalScheduleLogo,
@@ -27,7 +39,7 @@ const LogoTicker = () => {
                 width={150}
               />
             ))}
-          </div>
+          </motion.div>
         </div>
       </div>
     </section>
