ebbf95f387d190370724f2488ec669369c0ce949
florMignini <flormignini29@gmail.com>
2024-12-02 19:34:59 -0300

    institution responsive measures

--- app/professional/institutions/page.tsx
+++ app/professional/institutions/page.tsx
@@ -66,15 +66,15 @@ const InstitutionsPage = async () => {
             {/*header*/}
             <div className="w-[99%] px-3 flex items-center justify-between border-b-[1px] mb-3 border-b-gray-500">
               <p className="w-[25%] max-[690px]:w-[50%] h-10 text-sm font-medium text-gradient text-start">
-                Institución
+                institucion
               </p>
-              <p className="sm:w-[25%] hidden md:flex max-[690px]:w-[50%] h-10 text-sm font-medium text-gradient text-start">
+              <p className="w-[25%] max-[690px]:w-[50%] h-10 text-sm font-medium text-gradient text-start">
                 Teléfono
               </p>
-              <p className="sm:w-[25%] hidden md:flex max-[690px]:w-[50%] h-10 text-sm font-medium text-gradient text-start">
+              <p className="w-[25%] h-10 text-sm font-medium text-gradient text-start max-[690px]:hidden">
                 Mail
               </p>
-              <p className="sm:w-[25%] hidden md:flex max-[690px]:w-[50%] h-10 text-sm font-medium text-gradient text-start max-[650px]:hidden">
+              <p className="w-[25%] h-10 text-sm font-medium text-gradient text-start max-[690px]:hidden">
                 Dirección
               </p>
             </div>
@@ -107,7 +107,7 @@ const InstitutionsPage = async () => {
                         </div>
                       </div>
                       <div
-                        className="hidden max-[690px]:w-[50%] w-[25%] px-1 py-2 md:flex items-center justify-start"
+                        className="w-[25%] max-[690px]:w-[50%] px-1 py-2"
                         key={institution.phone}
                       >
                         <div className="text-[14px] font-normal flex gap-1">
