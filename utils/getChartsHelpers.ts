bb71cb4ffaadf140167da65c89fbf8f3214398ab
florMignini <flormignini29@gmail.com>
2025-02-11 09:18:52 -0300

    Add filtering function for appointments by age range and gender

--- utils/getChartsHelpers.ts
+++ utils/getChartsHelpers.ts
@@ -1,6 +1,6 @@
 import { AppointmentsIncluded } from "@/interfaces";
 import dayjs from "dayjs";
-
+const today = new Date()
 export const filterTodayAppointments = (appointments: AppointmentsIncluded[]) => {
     const today = new Date().toDateString();
     let result = appointments.filter((appointment) => {
@@ -8,3 +8,14 @@ export const filterTodayAppointments = (appointments: AppointmentsIncluded[]) =>
     });
     return result;
 }
+
+
+// filter by age and gender fn
+export function filtrarByAgeRange(data: any, beginAge: number, endAge: number, gender: string) {
+
+    return data.filter(({patient} : any) => 
+      (today.getTime() - new Date(patient.birthDate).getTime()) / (1000 * 60 * 60 * 24 * 365) >= beginAge && 
+      (today.getTime() - new Date(patient.birthDate).getTime()) / (1000 * 60 * 60 * 24 * 365) <= endAge && 
+      patient.gender === gender
+    );
+  }
