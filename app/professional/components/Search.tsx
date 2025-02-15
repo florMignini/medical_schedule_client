302165354a3ff2f26ab3f47247bc42bf870b097c
florMignini <flormignini29@gmail.com>
2025-02-15 08:09:30 -0300

    Refactor Search component for improved result visual approach

--- app/professional/components/Search.tsx
+++ app/professional/components/Search.tsx
@@ -1,18 +1,18 @@
 "use client";
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useDebounce } from "@/utils";
 import { Input } from "@/components/ui/input";
-import Icon from "@/components/ui/icon";
+
 import searchIcon from "../../../public/assets/icons/search.svg";
 import { apiServer } from "@/api/api-server";
-import { Patient } from "@/interfaces";
+
 import Link from "next/link";
 import Image from "next/image";
 
 const Search = ({ path }: any) => {
   const [query, setQuery] = useState<string>("");
   const [loading, setLoading] = useState<boolean>(false);
-  const value:string = useDebounce(query);
+  const value: string = useDebounce(query);
   const [result, setResult] = useState<any>([]);
 
   //search
@@ -53,7 +53,7 @@ const Search = ({ path }: any) => {
         <div className="w-[100%] grid grid-cols-[15%,85%] align-middle justify-center bg-white rounded-md px-2 shadow-md shadow-[#cccccc] border-[1px] border-[#cccccc]">
           <button className="w-[90%] gap-2 flex items-center justify-center">
             <Image src={searchIcon} alt="search-icon" width={15} height={15} />
-          <div className="h-5 border-x-[1px] border-black/20" />
+            <div className="h-5 border-x-[1px] border-black/20" />
           </button>
           <Input
             type="text"
@@ -71,41 +71,41 @@ const Search = ({ path }: any) => {
           />
         </div>
 
- {/* result section */}
- <>
-        {value && (
-          <div className="absolute w-[90%] rounded-md py-5 px-2 z-40 top-[82px] gap-2 bg-backdrop backdrop-blur-[9px]">
-            {result && result?.length > 0 ? (
-              result?.map((patient: any) => (
-                <Link
-                  key={patient.id}
-                  href={`/professional/patients/${patient.id}/info`}
-                  className=" w-[99%] grid grid-cols-[50%,50%] align-middle justify-center my-2 px-1 py-2 hover:rounded-2xl hover:bg-gradient-to-b from-black to-[#545455] text-transparent hover:opacity-80 z-50"
-                  onClick={clearInput}
-                >
-                  <div className="w-[100%] flex items-center justify-center">
-                    <Image
-                      src={patient.patientPhotoUrl}
-                      alt="patient-photo"
-                      width={50}
-                      height={50}
-                      className="rounded-full grid items-center justify-center"
-                    />
-                  </div>
-                  <p className="text-[#bdbfb7] text-base flex items-center justify-start font-semibold">
-                    {`${patient.firstName} ${patient.lastName}`}
-                  </p>
-                </Link>
-              ))
-            ) : (
-              <p className="w-[100%] flex items-center justify-center text-base font-bold text-[#5653AF]">
-                {" "}
-                No se encontraron resultados
-              </p>
-            )}
-          </div>
-        )}
-      </>
+        {/* result section */}
+        <>
+          {value && (
+            <div className="w-[90%] absolute z-50 rounded-md py-5 px-2 top-[82px] gap-2 bg-backdrop backdrop-blur-[9px]">
+              {result && result?.length > 0 ? (
+                result?.map((patient: any) => (
+                  <Link
+                    key={patient.id}
+                    href={`/professional/patients/${patient.id}/info`}
+                    className=" w-[99%] grid grid-cols-[50%,50%] align-middle justify-center my-2 px-1 py-2 hover:rounded-2xl hover:bg-gradient-to-b from-[#525252] to-[#979798] text-transparent hover:opacity-80 z-50"
+                    onClick={clearInput}
+                  >
+                    <div className="w-[100%] flex items-center justify-center">
+                      <Image
+                        src={patient.patientPhotoUrl}
+                        alt="patient-photo"
+                        width={50}
+                        height={50}
+                        className="rounded-full grid items-center justify-center"
+                      />
+                    </div>
+                    <p className="text-black text-base flex items-center justify-start font-semibold">
+                      {`${patient.firstName} ${patient.lastName}`}
+                    </p>
+                  </Link>
+                ))
+              ) : (
+                <p className="w-[100%] flex items-center justify-center text-base font-bold text-">
+                  {" "}
+                  No se encontraron resultados
+                </p>
+              )}
+            </div>
+          )}
+        </>
       </div>
     </div>
   );
