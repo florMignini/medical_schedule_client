39f564120a4925e853ab673782d39d68ade04ca3
florMignini <flormignini29@gmail.com>
2025-02-20 08:39:01 -0300

    Update patient validation rules

--- lib/patientValidation.ts
+++ lib/patientValidation.ts
@@ -3,32 +3,35 @@ export const patientsRegisterValidation = z.object({
   // personal
   firstName: z
     .string()
-    .min(2, "El nombre debe tener al menos 2 caracteres")
+    .min(2, "El nombre debe tener al menos 6 caracteres")
     .max(50, "El nombre debe tener como máximo 50 caracteres"),
   lastName: z
     .string()
-    .min(2, "El apellido debe tener al menos 2 caracteres")
+    .min(2, "El apellido debe tener al menos 6 caracteres")
     .max(50, "El apellido debe tener como máximo 50 caracteres"),
   patientPhoto: z.custom<File[]>().optional(),
   address: z
     .string()
-    .min(2, "Dirección debe tener al menos 2 caracteres")
+    .min(2, "Dirección debe tener al menos 6 caracteres")
     .max(500, "Dirección debe tener como máximo 500 caracteres"),
   occupation: z
     .string()
-    .min(2, "Ocupación debe tener al menos 2 caracteres")
+    .min(2, "Ocupación debe tener al menos 6 caracteres")
     .max(500, "Ocupación debe tener al menos 500 caracteres"),
   email: z.string().email("Email inválido"),
   phone: z
     .string()
     .refine(
-      (phone) => /^\+\d{10,15}$/.test(phone),
+      (phone) => /^\+\d{10,15}$/.test(phone ?? ""),
       "Número de teléfono inválido"
     ),
     gender: z.enum(["M", "F", "X"]),
-    birthDate: z.coerce.date(),
+    birthDate: z.coerce.date({
+      required_error: "Por favor seleccione una fecha",
+      invalid_type_error: "That's not a date!",
+    }),
   identificationType: z.enum(['DNI', 'Libreta de Enrolamiento', 'Pasaporte']),
-  identityNumber: z.string().optional(),
+  identityNumber: z.string().min(6, "Número de Identificación debe tener al menos 6 caracteres"),
   emergencyContactName: z
     .string()
     .min(6, "Nombre de Contacto debe tener al menos 6 caracteres")
@@ -42,9 +45,9 @@ export const patientsRegisterValidation = z.object({
   // medical
   insuranceProvider: z
     .string()
-    .min(2, "Debe tener al menos 2 caracteres")
+    .min(2, "Debe tener al menos 6 caracteres")
     .max(50, "Debe tener como máximo 50 caracteres"),
-  insurancePolicyNumber: z.string(),
+  insurancePolicyNumber: z.string().min(6, "Número de Póliza debe tener al menos 6 caracteres"),
   smoker: z.enum(["SI", "NO"]),
   exSmoker: z.enum(["SI", "NO"]),
   bloodType: z.enum(["A", "B", "AB", "O"]),
@@ -71,20 +74,20 @@ export const patientsRegisterValidation = z.object({
 export const patientsUpdateValidationSchema = z.object({
   firstName: z
   .string()
-  .min(2, "El nombre debe tener al menos 2 caracteres")
+  .min(2, "El nombre debe tener al menos 6 caracteres")
   .max(50, "El nombre debe tener como máximo 50 caracteres").optional(),
 lastName: z
   .string()
-  .min(2, "El apellido debe tener al menos 2 caracteres")
+  .min(2, "El apellido debe tener al menos 6 caracteres")
   .max(50, "El apellido debe tener como máximo 50 caracteres").optional(),
 patientPhoto: z.custom<File[]>().optional(),
 address: z
   .string()
-  .min(2, "Dirección debe tener al menos 2 caracteres")
+  .min(2, "Dirección debe tener al menos 6 caracteres")
   .max(500, "Dirección debe tener como máximo 500 caracteres").optional(),
 occupation: z
   .string()
-  .min(2, "Ocupación debe tener al menos 2 caracteres")
+  .min(2, "Ocupación debe tener al menos 6 caracteres")
   .max(500, "Ocupación debe tener al menos 500 caracteres").optional(),
 email: z.string().email("Email inválido").optional(),
 phone: z
@@ -95,22 +98,22 @@ phone: z
   ).optional(),
 birthDate: z.coerce.date().optional(),
 gender: z.enum(["M", "F", "X"]).optional(),
-identificationType: z.enum(['DNI', 'Libreta de Enrolamiento', 'Pasaporte']).optional(),
-identityNumber: z.string().optional(),
+identificationType: z.enum(['DNI', 'Libreta de Enrolamiento', 'Pasaporte']),
+identityNumber: z.string().min(6, "Número de Identificación debe tener al menos 6 caracteres").optional(),
 emergencyContactName: z
   .string()
   .min(6, "Nombre de Contacto debe tener al menos 6 caracteres")
   .max(50, "Nombre de Contacto debe tener como máximo 50 caracteres").optional(),
 emergencyContactNumber: z
   .string().optional()
-  /* .refine(
-    (emergencyContactNumber) => /^\+\d{10,15}$/.test(emergencyContactNumber),
-    "Invalid phone number"
-  ) */,
+  .refine(
+    (phone) => /^\+\d{10,15}$/.test(phone ?? ""),
+    "Número de teléfono inválido"
+  ).optional(), 
 // medical
 insuranceProvider: z
   .string()
-  .min(2, "Debe tener al menos 2 caracteres")
+  .min(2, "Debe tener al menos 6 caracteres")
   .max(50, "Debe tener como máximo 50 caracteres").optional(),
 insurancePolicyNumber: z.string().optional(),
 smoker: z.enum(["SI", "NO"]).optional(),
