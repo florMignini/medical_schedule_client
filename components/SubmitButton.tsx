3f93326de548a7a92fd2740ef6b7c33f89d387c9
florMignini <flormignini29@gmail.com>
2025-02-28 07:51:27 -0300

    Refactor SubmitButton component to support optional disabled prop and improve loading state handling

--- components/SubmitButton.tsx
+++ components/SubmitButton.tsx
@@ -2,17 +2,17 @@ import Image from 'next/image';
 import spinner from '../public/assets/icons/loader.svg'
 
 interface SubmitButtonI {
-    loading: boolean;
+    loading?: boolean;
     className?: string;
     children: React.ReactNode;
-  
+  disabled?: boolean;
 }
 
-const SubmitButton = ({loading, className, children}: SubmitButtonI) => {
+const SubmitButton = ({loading, className, children, disabled}: SubmitButtonI) => {
   return (
    <button
    type='submit'
-   disabled={loading}
+   disabled={disabled ? disabled : loading}
    className={`text-sm font-medium tracking-w-normal text-black transition duration-200 ease-in-out ${className}`}
    >
     {loading ? (
