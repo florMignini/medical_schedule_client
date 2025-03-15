7c23404f40c7fb7c75b8ed429516e6b116eefd5d
florMignini <flormignini29@gmail.com>
2025-03-15 10:02:00 -0300

    Refactor Appointments page layout to include AppointmentsList component and improve responsiveness

--- app/professional/appointments/page.tsx
+++ app/professional/appointments/page.tsx
@@ -1,47 +1,54 @@
-import Icon from "@/components/ui/icon"
 import { cookies } from "next/headers";
 import { apiServer } from "@/api/api-server";
 import { AppointmentsIncluded, ProfessionalInformation } from "@/interfaces";
-import calendar from "../../../public/assets/icons/appointments.svg"
-import Link from "next/link";
+
 import Calendar from "../components/Calendar";
 import CalendarIcon from "../components/icons/CalendarIcon";
+import AppointmentsList from "../components/AppointmentsList";
 
-const Appointments = async() => {
-    const cookieStore = cookies();
+const Appointments = async () => {
+  const cookieStore = cookies();
   const professionalId = cookieStore.get("professional-id")?.value;
 
   let { data }: { data: ProfessionalInformation } = await apiServer.get(
     `/professional/get-professional/${professionalId}`
   );
-// @ts-ignore
-  const { appointmentsIncluded }: { appointmentsIncluded: AppointmentsIncluded[] } = data ?? { appointmentsIncluded: [] };
+  // @ts-ignore
+  const {
+    appointmentsIncluded,
+  }: { appointmentsIncluded: AppointmentsIncluded[] } = data ?? {
+    appointmentsIncluded: [],
+  };
+
   return (
-    <section className="w-full h-screen flex flex-col items-center justify-start gap-2 text-color">
-         {/* Title */}
+    <section className="w-full min-h-screen flex flex-col items-center justify-start gap-2 text-color">
+      {/* Title */}
       <div className="flex w-[90%] h-10 items-start justify-start px-2">
         <h1 className="text-18-bold text-start">Reservaciones</h1>
       </div>
       {/* top section */}
       <div className="w-[90%] flex items-center justify-start gap-2">
-       <CalendarIcon
-       width={20}
-       height={20}
-       className="text-color"
-       />
-       <div className="flex items-center justify-center gap-1">
-        <h1 className="text-18-bold ">{appointmentsIncluded.length}</h1>
-        <p className="text-18-bold">{appointmentsIncluded.length >1 ? `Citas totales` : `Cita total`}</p>
-       </div>
+        <CalendarIcon width={20} height={20} className="text-color" />
+        <div className="flex items-center justify-center gap-1">
+          <h1 className="text-18-bold ">{appointmentsIncluded.length}</h1>
+          <p className="text-18-bold">
+            {appointmentsIncluded.length > 1 ? `Citas totales` : `Cita total`}
+          </p>
+        </div>
       </div>
       {/* Calendar section */}
-      <div className="w-full flex items-center justify-center h-auto py-4">
-      <Calendar
-      appointments={appointmentsIncluded}
-      />
+      <div className="w-full grid grid-cols-[50,50] h-auto py-4 min-[768px]:flex min-[768px]:flex-row  xl:gap-8">
+        <div className="w-[100%]">
+        <Calendar 
+        appointments={appointmentsIncluded} />
+        </div>
+        {/* Lista de eventos */}
+        <div className="w-[100%] flex items-start justify-start">
+        <AppointmentsList appointments={appointmentsIncluded} />
+        </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Appointments
+export default Appointments;
