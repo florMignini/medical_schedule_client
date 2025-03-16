8f1cbbd7b319dc75359fdc76ba411c93fcbc8fe1
florMignini <flormignini29@gmail.com>
2025-03-16 19:07:45 -0300

    Update getTodayAppointments function to accept scheduleDate parameter for filtering

--- utils/getTodayAppointments.ts
+++ utils/getTodayAppointments.ts
@@ -1,11 +1,14 @@
-export const getTodayAppointments = (appointments:any) => {
-const todayAppointments = appointments.filter((appointment:any) => {
+
+import { Appointment, AppointmentsIncluded } from "@/interfaces";
+
+export const getTodayAppointments = (appointments: Appointment[], scheduleDate: Date) => {
+const todayAppointments = (appointments).filter((appointment: any) => {
     const appointmentDate = new Date(appointment.appointment.schedule);
     const today = new Date();
     return (
-    appointmentDate.getDate() === today.getDate() &&
-    appointmentDate.getMonth() === today.getMonth() &&
-    appointmentDate.getFullYear() === today.getFullYear()
+    appointmentDate.getDate() === scheduleDate.getDate() &&
+    appointmentDate.getMonth() === scheduleDate.getMonth() &&
+    appointmentDate.getFullYear() === scheduleDate.getFullYear()
     );
 });
 return todayAppointments;
