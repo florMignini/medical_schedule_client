eeb64f143cec84168ee45de1b317a6c76677ad48
florMignini <flormignini29@gmail.com>
2025-03-17 19:04:46 -0300

    Add getAppointmentDetail function to retrieve appointment details by ID

---
+++ utils/getAppointmentDetail.ts
@@ -0,0 +1,28 @@
+//-----------------------------------------
+
+import { apiServer } from "@/api/api-server";
+import { Patient } from "@/interfaces";
+interface appointmentResult {
+  cancellationReason: string | null;
+  createdAt: string;
+  id: string;
+  notes: string;
+  patientsIncluded: Patient[];
+  reason: string;
+  schedule: string;
+  medicalInstitutionId: string | null;
+  updatedAt: string;
+}
+// get appointment detail action
+export const getAppointmentDetail = async (
+  appointmentId: string
+): Promise<appointmentResult | undefined> => {
+  try {
+    const { data } = await apiServer(
+      `/appointment/get-appointment/${appointmentId}`
+    );
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+};
