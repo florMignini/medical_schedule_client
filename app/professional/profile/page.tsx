308713fcc6627635c73b654ff27a7c2642635871
florMignini <flormignini29@gmail.com>
2025-04-04 08:47:01 -0300

    Update Profile component layout for improved responsiveness and structure

--- app/professional/profile/page.tsx
+++ app/professional/profile/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import WelcomeSection from '../components/WelcomeSection'
 import { cookies } from 'next/headers';
+// import { ScrollArea } from "../../../components/ui/scroll-area";
 import { ProfessionalInformation } from '@/interfaces';
 import { apiServer } from '@/api/api-server';
 
@@ -14,8 +15,10 @@ const Profile = async() => {
       );
 
   return (
-    <section className="w-full h-screen flex flex-col items-center justify-start gap-2">
+    <section className="w-full h-auto flex flex-col items-center justify-start gap-2 py-1">
+        {/* <ScrollArea className='w-full h-full'> */}
         <WelcomeSection professional={data}/>
+        {/* </ScrollArea> */}
     </section>
   )
 }
