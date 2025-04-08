7f35df7695bb36560d02d397c30ac0b2efa909b1
florMignini <flormignini29@gmail.com>
2025-04-08 14:22:33 -0300

    Refactor PatientProfileUpdateForm to use patientInfo prop and improve default value handling

--- app/professional/patients/[patientId]/update/page.tsx
+++ app/professional/patients/[patientId]/update/page.tsx
@@ -26,7 +26,7 @@ const PatientUpdate = () => {
     <section className="w-full h-screen flex flex-col items-center justify-start gap-2">
     <ScrollArea className="h-[98%] w-[99%] rounded-md border border-dark-500 p-4">
     <PatientProfileUpdateForm
-    {...patientInfo}
+      patientInfo={patientInfo}
     />
     </ScrollArea>
   </section>
