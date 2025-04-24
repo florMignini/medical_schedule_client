89daf596e83479c8f1338e0ca2a7a8dacd23a17e
florMignini <flormignini29@gmail.com>
2025-04-24 07:55:21 -0300

    Update SelectedDateContext for selectedDate values

--- app/context/SeletedDateContext.tsx
+++ app/context/SeletedDateContext.tsx
@@ -1,14 +1,14 @@
 import { createContext, useContext, useState, ReactNode } from "react";
 
 interface SelectedDateContextProps {
-  selectedDate: Date;
-  setSelectedDate: (date: Date) => void;
+  selectedDate: Date | null;
+  setSelectedDate: (date: Date | null) => void;
 }
 
 export const SelectedDateContext = createContext<SelectedDateContextProps | undefined>(undefined);
 
 export function SelectedDateProvider({ children }: { children: ReactNode }) {
-  const [selectedDate, setSelectedDate] = useState<Date>(new Date());
+  const [selectedDate, setSelectedDate] = useState<Date | null>(null);
 
   return (
     <SelectedDateContext.Provider value={{ selectedDate, setSelectedDate }}>
