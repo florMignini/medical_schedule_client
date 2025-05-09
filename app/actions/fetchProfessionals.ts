13e59e62c7e8e049889178566666b77e822caad2
florMignini <flormignini29@gmail.com>
2025-05-09 10:49:19 -0300

    Refactor fetchProfessionals function: simplify API call by using a direct GET method and remove unnecessary headers and logging.

--- app/actions/fetchProfessionals.ts
+++ app/actions/fetchProfessionals.ts
@@ -5,13 +5,8 @@ import { apiServer } from "@/api/api-server";
 export async function fetchProfessionals() {
   "use server";
   try {
-    const res = await apiServer(`/patients/get-all-professionals`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    console.log(res);
+    const res = await apiServer.get(`/professionals/get-all-professionals`);
+
     return res;
   } catch (error) {
     console.error(error);
