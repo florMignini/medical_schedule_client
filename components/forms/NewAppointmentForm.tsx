ec99a6841dbc4122fba1f52aa3aa06a0409d1da3
florMignini <flormignini29@gmail.com>
2025-05-14 17:18:18 -0300

    Enhance NewAppointmentForm component: Implement patient fetching on mount, integrate patient selection dropdown with dynamic loading, and update appointment submission to include selected patient ID. Improve UI with loading skeleton for patient list.

--- components/forms/NewAppointmentForm.tsx
+++ components/forms/NewAppointmentForm.tsx
@@ -15,7 +15,19 @@ import {
   createProfessionalAppointmentRelation,
 } from "@/app/actions";
 import { useToast } from "@/hooks/use-toast";
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "../ui/select";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
+import { getAllPatients } from "@/utils/getAllPatients";
+import { Patient } from "@/interfaces";
+import Image from "next/image";
+import { ScrollArea } from "../ui/scroll-area";
+import { FormField, FormItem } from "../ui/form";
+import { Skeleton } from "@/components/ui/skeleton";
 
 type AppointmentType = "create" | "cancel" | "schedule";
 export type professionalDataType = {
@@ -43,6 +55,7 @@ const NewAppointmentForm = ({
 }) => {
   const [loading, setLoading] = useState(false);
   const [professionalId, setProfessionalId] = useState<professionalDataType>();
+  const [patients, setPatients] = useState<any[]>([]);
   const appointmentValidation = getAppointmentSchema(type);
   useEffect(() => {
     const professionalData = localStorage.getItem("infoProfSession");
@@ -51,6 +64,23 @@ const NewAppointmentForm = ({
       setProfessionalId(parsedData);
     }
   }, []);
+
+  // Fetch patients only when component is mounted
+  useEffect(() => {
+    const fetchPatients = async () => {
+      try {
+        const patientsData = await getAllPatients();
+        setPatients(patientsData as Patient[]);
+      } catch (error) {
+        console.error("Error fetching patients:", error);
+      }
+    };
+
+    if (component === "calendar") {
+      fetchPatients();
+    }
+  }, [component]);
+
   const { toast } = useToast();
   const router = useRouter();
   const form = useForm<z.infer<typeof appointmentValidation>>({
@@ -60,17 +90,22 @@ const NewAppointmentForm = ({
       reason: "",
       notes: "",
       cancellationReason: "",
+      patientId: "",
     },
   });
+
   async function onSubmit(values: z.infer<typeof appointmentValidation>) {
     setLoading(true);
+
     try {
       const appointmentData = {
         schedule: new Date(values.schedule),
         reason: values.reason,
         notes: values.notes,
       };
-      const response = await createAppointment(appointmentData) as AppointmentResponse;
+      const response = (await createAppointment(
+        appointmentData
+      )) as AppointmentResponse;
 
       if (response) {
         const professionalIDs = {
@@ -81,7 +116,7 @@ const NewAppointmentForm = ({
           professionalIDs
         );
         const patientsIDs = {
-          patient: patientId,
+          patient: values.patientId,
           appointment: response?.id,
         };
 
@@ -122,56 +157,90 @@ const NewAppointmentForm = ({
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
-        
         {type !== "cancel" && (
           <>
-          {component === "calendar" && (
-            <div>
-              <h3>Seleccione paciente:</h3>
-              <Select>
-                <SelectTrigger>
-                  <SelectValue placeholder="Seleccione paciente" />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value="1">Paciente 1</SelectItem>
-                  <SelectItem value="2">Paciente 2</SelectItem>
-                  <SelectItem value="3">Paciente 3</SelectItem>
-                </SelectContent>
-              </Select>
-            </div>
-          )}
+            {component === "calendar" && (
+              <div className="w-full flex flex-col text-gray-500 gap-2 items-center justify-center bg-inherit">
+                <h3 className="w-full font-thin text-lg">Pacientes pertenecientes a su cartera:</h3>
+                {patients.length > 0 ? (
+                  <FormField
+                    control={form.control}
+                    name="patientId"
+                    render={({ field }) => (
+                      <FormItem className="w-[75%] mx-auto">
+                        <Select
+                          onValueChange={field.onChange}
+                          defaultValue={field.value}
+                        >
+                          <SelectTrigger className="h-16">
+                            <SelectValue placeholder="Seleccione paciente" />
+                          </SelectTrigger>
+                          <ScrollArea className="w-full max-h-[300px]">
+                            <SelectContent className="flex flex-col gap-2 items-center justify-center bg-black/10 backdrop-blur-lg">
+                              {patients.map((patient: Patient) => (
+                                <SelectItem
+                                  key={patient.id}
+                                  value={patient.id}
+                                  className="text-white h-16"
+                                >
+                                  <div className="flex gap-2 items-center justify-center">
+                                    <Image
+                                      src={patient.patientPhotoUrl}
+                                      alt={patient.firstName}
+                                      width={50}
+                                      height={50}
+                                      className="rounded-full"
+                                    />
+                                    <h2 className="text-white font-bold text-base">
+                                      {patient.firstName} {patient.lastName}
+                                    </h2>
+                                  </div>
+                                </SelectItem>
+                              ))}
+                            </SelectContent>
+                          </ScrollArea>
+                        </Select>
+                      </FormItem>
+                    )}
+                  />
+                ) : (
+                  <div className="w-full space-y-2">
+                    <Skeleton className="h-16 bg-white/20 w-[75%] rounded-md" />
+                  </div>
+                )}
+              </div>
+            )}
             {/* pick date */}
             <div className="flex flex-col gap-1">
               <label className="text-gray-500">Fecha del turno</label>
-            <DinamicForm
-              fieldType={FormFieldType.DATE_PICKER}
-              control={form.control}
-              name="schedule"
-              showTimeSelect
-              defaultValue={initialDateTime || new Date()}
-              dateFormat="dd/MM/yyyy - h:mm aa"
-            />
+              <DinamicForm
+                fieldType={FormFieldType.DATE_PICKER}
+                control={form.control}
+                name="schedule"
+                showTimeSelect
+                defaultValue={initialDateTime || new Date()}
+                dateFormat="dd/MM/yyyy - h:mm aa"
+              />
             </div>
             <div className="flex flex-col gap-5 xl:flex-row">
-             <div className="flex flex-col gap-1">
-              <label className="text-gray-500">Motivo de la consulta</label>
-             <DinamicForm
-                fieldType={FormFieldType.TEXTAREA}
-                control={form.control}
-                name="reason"
-                placeholder="Ingrese el motivo de la consulta"
-              />
-             </div>
-             <div className="flex flex-col gap-1">
-              <label className="text-gray-500">Información adicional</label>
-              <DinamicForm
-                fieldType={FormFieldType.TEXTAREA}
-                control={form.control}
-                name="notes"
-                placeholder="Ingrese información adicional"
-              />
-             </div>
-              
+              <div className="flex flex-col gap-1">
+                <label className="text-gray-500">Motivo de la consulta</label>
+                <DinamicForm
+                  fieldType={FormFieldType.TEXTAREA}
+                  control={form.control}
+                  name="reason"
+                  placeholder="Ingrese el motivo de la consulta"
+                />
+              </div>
+              <div className="flex flex-col gap-1">
+                <label className="text-gray-500">Información adicional</label>
+                <DinamicForm
+                  fieldType={FormFieldType.TEXTAREA}
+                  control={form.control}
+                  name="notes"
+                  placeholder="Ingrese información adicional"
+                />
+              </div>
             </div>
           </>
         )}
