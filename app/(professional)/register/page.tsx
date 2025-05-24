3377bdee5b179393ae42064672d93440413a53cc
florMignini <flormignini29@gmail.com>
2025-05-24 09:45:33 -0300

    Add debug logging for search parameters in ProfessionalRegistration page: Introduce a console log to display search parameters, aiding in troubleshooting and development during the registration process.

--- app/(professional)/register/page.tsx
+++ app/(professional)/register/page.tsx
@@ -2,6 +2,7 @@ import ProfessionalRegistration from "./professional-registration/page";
 
 
 const Page = ({ searchParams }: { searchParams: { token?: string } }) => {
+  console.log("Search Params:", searchParams);
   return <ProfessionalRegistration token={searchParams.token} />;
 };
 
