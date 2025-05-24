7956ccb3b8d68a604c5ec9ac601792122151a560
florMignini <flormignini29@gmail.com>
2025-05-24 06:58:31 -0300

    Implement token validation in ProfessionalRegistration component: Add loading state, error handling, and display email upon successful validation.

--- app/(admin)/admin/admin-panel/professional-registration/page.tsx
+++ app/(admin)/admin/admin-panel/professional-registration/page.tsx
@@ -1,8 +1,49 @@
+"use client";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 import { ScrollArea } from "../../../../../components/ui/scroll-area";
 import ProfessionalRegistrationForm from "../../components/forms/ProfessionalRegistrationForm";
+import { validateToken } from "@/app/actions";
+import { Skeleton } from "@/components";
 
+interface TokenValidationResponse {
+  status: number;
+  data: {
+    email: string;
+  };
+  message: string;
+}
 
 const ProfessionalRegistration = () => {
+  const router = useRouter();
+  const { token } = router.query;
+
+  const [email, setEmail] = useState('');
+  const [loading, setLoading] = useState(true);
+  const [tokenValid, setTokenValid] = useState(false);
+  const [error, setError] = useState('');
+  useEffect(() => {
+    (async () => {
+      if (!token) return;
+      try {
+        const response = await validateToken(token as string) as TokenValidationResponse;
+        if (response.status === 200) {
+          setEmail(response.data.email);
+          setTokenValid(true);
+        } else {
+          setError(response.message);
+        }
+      } catch (err: any) {
+        console.error("Error validating token:", err);
+        setError("Error validating token, please try again.");
+      } finally {
+        setLoading(false);
+      }
+    })
+  }, [token]);
+  if (loading) return <Skeleton className="h-36 bg-gray-500 w-[95%] rounded-md" />;
+  if (!tokenValid) return <p className="text-red-500">Invalid or expired token.</p>;
+  if (error) return <p className="text-red-500">{error}</p>;
   return (
     <section className="w-full h-screen flex pt-2 flex-col items-center justify-start gap-2">
       <ScrollArea className="h-[98%] w-[99%] rounded-md border border-dark-500 p-4">
