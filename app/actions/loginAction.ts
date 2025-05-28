242d2cfc572ca262e4865b3777b9b317a8ae2e20
florMignini <flormignini29@gmail.com>
2025-05-27 22:26:03 -0300

    minor chages

--- app/actions/loginAction.ts
+++ app/actions/loginAction.ts
@@ -17,7 +17,7 @@ export async function loginUser(loginData: IloginData) {
   "use server";
   try {
     const { data } = await apiServer.post<LoginUserResponse>(`https://medical-schedule-server.onrender.com/api/auth/login`, loginData);
-   console.log(data);
+
     cookies().set("session-token", data?.accessToken, { secure: true });
     cookies().set("professional-id", data?.professional.id, { secure: true });
     return data.professional;
