1e1877340782c6d448ce601e32d45cf98fc358e1
florMignini <flormignini29@gmail.com>
2025-06-18 19:43:01 -0300

    Refactor follow-up actions to improve error handling and switch to axios for patient and professional follow-up relations

--- app/actions/followUpAction.ts
+++ app/actions/followUpAction.ts
@@ -1,6 +1,7 @@
 "use server";
 import { apiServer } from "@/api/api-server";
 import { ICreateFollowUp } from "@/interfaces/createFollowUp.interface";
+import axios from "axios";//only for testing
 interface IpatientIDs {
   patient: string | undefined;
   followUp: string | undefined;
@@ -16,20 +17,38 @@ interface IFollowUp {
 export default async function createFollowUp(followUpData: ICreateFollowUp) {
   "use server";
   try {
-    console.log(followUpData);
     const { data } = await apiServer.post(
       `/follow-up/new-follow-up`,
       followUpData
     );
-    return data;
-  } catch (error:any) {
-      console.log(error?.response?.data);
+    return { success: true, data };
+  } catch (error: any) {
+    const status = error?.response?.status;
+    const message = error?.response?.data?.message;
+
+    console.error("FollowUp error:", message);
+
+    // Podés actuar según el código
+    if (status === 409) {
+      return {
+        success: false,
+        error: "Ya existe un seguimiento para la fecha indicada",
+        status,
+      };
+    }
+
+    return {
+      success: false,
+      error: message || "Error inesperado al crear el seguimiento",
+      status: status || 500,
+    };
   }
 }
 
+
 export async function createPatientFollowUpRelation(IDs: IpatientIDs) {
   try {
-  const res = await apiServer.post(`/patients/add-follow-up-relation`,IDs);
+  const res = await axios.post(`http://localhost:3001/patients/add-follow-up-relation`,IDs);
 } catch (error: any) {
   console.error(error?.response?.data);
 }  
@@ -37,7 +56,7 @@ export async function createPatientFollowUpRelation(IDs: IpatientIDs) {
 
 export async function createProfessionalFollowUpRelation(IDs: IIDs) {
  try {
-   const res = await apiServer.post(`/professional/add-follow-up-relation`,IDs);
+   const res = await axios.post(`http://localhost:3001/professional/add-follow-up-relation`,IDs);
  } catch (error: any) {
   console.error(error?.response?.data);
 } 
@@ -45,7 +64,7 @@ export async function createProfessionalFollowUpRelation(IDs: IIDs) {
 
 export async function createAppointmentFollowUpRelation(IDs: IFollowUp) {
 try {
-  const res = await apiServer.post(`/appointment/add-follow-up-relation`,IDs);
+  const res = await axios.post(`http://localhost:3001/appointment/add-follow-up-relation`,IDs);
 } catch (error: any) {
   console.error(error?.response?.data);
 } 
