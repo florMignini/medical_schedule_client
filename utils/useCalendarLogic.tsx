03d7bc2437617da6fda45cb97a26f9c665693d86
florMignini <flormignini29@gmail.com>
2025-06-25 18:45:57 -0300

    Add custom hook useCalendarLogic for managing calendar state and navigation

---
+++ utils/useCalendarLogic.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import dayjs from "dayjs";
+import "dayjs/locale/es";
+dayjs.locale("es");
+
+import { useSelectedDate } from "@/utils/useSelectedDate";
+import { useHolidays } from "@/hooks/useHolidays";
+
+export const useCalendarLogic = () => {
+  const { selectedDate, setSelectedDate } = useSelectedDate();
+  const [currentMonth, setCurrentMonth] = useState(dayjs().month());
+  const [currentYear, setCurrentYear] = useState(dayjs().year());
+  const holidays = useHolidays();
+
+  const daysInMonth = dayjs(`${currentYear}-${currentMonth + 1}`).daysInMonth();
+  const startDay = dayjs(`${currentYear}-${currentMonth + 1}-01`).day();
+  const today = dayjs(new Date());
+
+  const handlePrevMonth = () => {
+    setCurrentMonth((prev) => (prev === 0 ? 11 : prev - 1));
+    if (currentMonth === 0) setCurrentYear((prev) => prev - 1);
+  };
+
+  const handleNextMonth = () => {
+    setCurrentMonth((prev) => (prev === 11 ? 0 : prev + 1));
+    if (currentMonth === 11) setCurrentYear((prev) => prev + 1);
+  };
+
+  return {
+    selectedDate,
+    setSelectedDate,
+    currentMonth,
+    currentYear,
+    handlePrevMonth,
+    handleNextMonth,
+    daysInMonth,
+    startDay,
+    today,
+    holidays,
+  };
+};
