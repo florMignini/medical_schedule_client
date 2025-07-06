b6e102bec9b32447ef3fd55f879e52133dab3413
florMignini <flormignini29@gmail.com>
2025-07-06 20:12:17 -0300

    Update InstitutionsPage header: Revise header layout to improve visual hierarchy and responsiveness, adding a descriptive subtitle for better user guidance.

--- app/(professional)/professional/institutions/page.tsx
+++ app/(professional)/professional/institutions/page.tsx
@@ -4,9 +4,14 @@ import { InstitutionList } from "./components/InstitutionList";
 export default function InstitutionsPage() {
   return (
     <section className="w-[99%] mx-auto p-6 space-y-4 bg-white rounded-lg shadow-md h-screen">
-      <header className="flex items-center justify-between">
-        <h2 className="text-2xl font-semibold">Instituciones</h2>
-      </header>
+       <header className="flex flex-col w-[100%] h-14 items-start justify-center px-2 border-b-[1px] border-b-gray-500">
+            <h1 className="text-2xl text-black font-semibold text-start">
+              Instituciones
+            </h1>
+            <p className="hidden md:flex text-xs font-light text-gray-600">
+              Aquí encontrara la lista de instituciones que se hallan en su cartera
+            </p>
+          </header>
 
       <InstitutionList />
     </section>
