e6821be9ea286f57fca720ba935f0940b1795f37
florMignini <flormignini29@gmail.com>
2025-07-09 10:29:57 -0300

    Refactor Appointments component: Replace manual data fetching with useProfessionalIncludes hook, streamline loading state handling, and enhance UI with a skeleton loader for improved user experience.

--- app/(professional)/professional/appointments/page.tsx
+++ app/(professional)/professional/appointments/page.tsx
@@ -3,65 +3,56 @@
 import { useState, useEffect } from "react";
 import Calendar from "../components/Calendar";
 import AppointmentsList from "../components/AppointmentsList";
-import { fetchProfessionalAppointments } from "@/utils/fetchProfessionalAppointments";
 import CalendarIcon from "../components/icons/CalendarIcon";
 import { useCurrentProfessional } from "@/hooks/useCurrentProfessional";
+import { useProfessionalIncludes } from "@/hooks/useProfessionalIncludes";
+import { AppointmentSkeletonLoader } from "./components/AppointmentSkeletonLoader";
 
 const Appointments = () => {
-  const [appointmentsIncluded, setAppointmentsIncluded] = useState<any[]>([]);
-  const [patientsIncluded, setPatientsIncluded] = useState<any[]>([]);
   const [loading, setLoading] = useState(true);
   const professional = useCurrentProfessional();
-  useEffect(() => {
-    async function loadData() {
-        const professionalId = professional?.id!;
-
-      try {
-        const { appointmentsIncluded, patientsIncluded } = await fetchProfessionalAppointments(professionalId);
-        setAppointmentsIncluded(appointmentsIncluded);
-        setPatientsIncluded(patientsIncluded);
-      } catch (error) {
-        console.error("Error cargando datos", error);
-      } finally {
-        setLoading(false);
-      }
-    }
-    loadData();
-  }, []);
-
+  const { appointments, patients, isLoading, refetch } =
+    useProfessionalIncludes();
   return (
-    <section className="w-[99%] mx-auto p-6 space-y-4 bg-white rounded-lg shadow-md h-screen">
+    <section className="w-[99%] mx-auto p-6 space-y-4 bg-white rounded-lg shadow-md h-[100%]">
       {/* Header */}
       <header className="flex flex-col w-[100%] h-14 items-start justify-center px-2 border-b-[1px] border-b-gray-500">
-      <h1 className="text-2xl text-black font-semibold text-start">
-              Calendario
-            </h1>
-            <p className="hidden md:flex text-xs font-light text-gray-600">
-            Aquí encontrará los turnos programados para el mes seleccionado
-            </p>
-          </header>
+        <h1 className="text-2xl text-black font-semibold text-start">
+          Calendario
+        </h1>
+        <p className="hidden md:flex text-xs font-light text-gray-600">
+          Aquí encontrará los turnos programados para el mes seleccionado
+        </p>
+      </header>
 
-      {/* Info Top */}
-      <div className="w-full flex items-center gap-3 text-gray-700">
-        <CalendarIcon width={24} height={24} className="text-gray-600" />
-        <h2 className="text-lg font-semibold">
-          {appointmentsIncluded.length} {appointmentsIncluded.length > 1 ? "Citas totales" : "Cita total"}
-        </h2>
-      </div>
+      {isLoading ? (
+        <AppointmentSkeletonLoader />
+      ) : (
+        <>
+          <div className="w-full flex items-center gap-3 text-gray-700">
+            <CalendarIcon width={24} height={24} className="text-gray-600" />
+            <h2 className="text-lg font-semibold">
+              {appointments.length}{" "}
+              {appointments.length > 1 ? "Citas totales" : "Cita total"}
+            </h2>
+          </div>
 
-      {/* Main Content */}
-      <main className="w-full grid grid-cols-1 md:grid-cols-2 gap-8">
-        <div className="glass-effect-vibrant rounded-lg p-4 shadow-inner">
-          <Calendar appointments={appointmentsIncluded} />
-        </div>
-        <div className="glass-effect-vibrant rounded-lg p-4 shadow-inner">
-          <AppointmentsList
-            patients={patientsIncluded}
-            appointments={appointmentsIncluded}
-            pastAppointmentPatientData={[]} // Ajustar si lo necesitás
-          />
-        </div>
-      </main>
+          {/* Main Content */}
+          <main className="w-full grid grid-cols-1 md:grid-cols-2 gap-8">
+            <div className="glass-effect-vibrant rounded-lg p-4 shadow-inner">
+              <Calendar appointments={appointments} />
+            </div>
+            <div className="glass-effect-vibrant rounded-lg p-4 shadow-inner">
+              <AppointmentsList
+                patients={patients}
+                appointments={appointments}
+                pastAppointmentPatientData={[]} 
+                refetch={refetch}
+              />
+            </div>
+          </main>
+        </>
+      )}
     </section>
   );
 };
