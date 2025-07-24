c84a48999726abb841d14c4ae8c7e549ddf154ec
florMignini <flormignini29@gmail.com>
2025-07-24 08:54:21 -0300

    minor changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,45 +1,51 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'cloud.appwrite.io',
-                port: '',
-                pathname: '/**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'static.vecteezy.com',
-                port: '',
-                pathname: '/**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'img.freepik.com',
-                port: '',
-                pathname: '/**',
-            },
-            {
-                protocol: "https",
-                hostname: "avatar.iran.liara.run",
-                port: "",
-                pathname: "/**"
-            },
-                {
-                    protocol: "https",
-                    hostname: "randomuser.me",
-                    port: "",
-                    pathname: "/**"
-                },
-                {
-                    protocol: "https",
-                    hostname: "https://cloud.appwrite.io",
-                    port: "",
-                    pathname: "/**"
-                }
-        ],
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cloud.appwrite.io",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "via.placeholder.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "static.vecteezy.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "img.freepik.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "avatar.iran.liara.run",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "randomuser.me",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "https://cloud.appwrite.io",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
