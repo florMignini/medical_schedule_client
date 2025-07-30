1de56d880f3591b37f92ccbdcc2a66fad05f1824
florMignini <flormignini29@gmail.com>
2025-07-30 17:20:55 -0300

    Add demo login functionality in Header component: Implement handleDemoLogin to manage demo user authentication, store session data in localStorage and cookies, and provide user feedback via toast notifications.

--- app/(landing)/components/Header.tsx
+++ app/(landing)/components/Header.tsx
@@ -1,8 +1,61 @@
+"use client";
+import axios from "axios";
+import Cookies from "js-cookie";
 import Image from "next/image";
 import Logo from "@/public/assets/medical_schedule-transparent.png";
 import Link from "next/link";
+import { useRouter } from 'next/navigation';
+import { useToast } from '@/hooks/use-toast';
 
 const Header = () => {
+const router = useRouter();
+const {toast} = useToast()
+
+const handleDemoLogin = async () => {
+  console.log("🔐 Activando modo demo...");
+
+  try {
+    const response = await axios.post<{
+      access_token: string;
+      user: any;
+    }>("https://medical-schedule-server-demo.onrender.com/api/auth/demo-login");
+
+    const { access_token, user } = response.data;
+    // 👉 Guardar en localStorage
+    localStorage.setItem("session-token", access_token);
+    localStorage.setItem("user", JSON.stringify(user));
+
+    // 👉 Guardar en cookie (visible en middleware)
+    Cookies.set("session-token", access_token, {
+      path: "/",
+      sameSite: "Lax",
+      // opcional: secure: true, si solo sirve para HTTPS
+    });
+    Cookies.set("professional-id", user.id, {
+      path: "/",
+      sameSite: "Lax",
+    });
+    Cookies.set("isDemo", "true", {
+      path: "/",
+      sameSite: "Lax",
+    });
+    toast({
+      title: "Sesión iniciada en modo demo",
+      description: "Has accedido correctamente al modo demo.",
+      className: "bg-emerald-500 text-black",
+    });
+
+    router.push("/professional/dashboard");
+  } catch (error) {
+    toast({
+      title: "Error",
+      description: "No se pudo iniciar el modo demo",
+      className: "bg-red-500 text-white",
+    });
+    console.error(error);
+  }
+};
+
     return (
         <header className="w-full h-[60px] border-b border-white/15 sticky z-40 top-0 backdrop-blur-lg">
             {/* header */}
@@ -23,12 +76,12 @@ const Header = () => {
                     >
                         Soy usuario
                     </Link>
-                    <Link
-                        href="/admin"
+                    <button
+                    onClick={handleDemoLogin}
                         className="transition duration-200 ease-in-out flex items-center font-semibold justify-center gap-2.5 px-1 py-2   bg-gradient-to-r from-gray-500 to-gray-400 bg-clip-text text-transparent"
                     >
-                        Registrarme
-                    </Link>
+                        Demo access
+                    </button>
                 </div>
             </div>
         </header>
