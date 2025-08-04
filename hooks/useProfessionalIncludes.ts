af7c979a12bb94ea77b1ca15a0040fed73ddcb35
florMignini <flormignini29@gmail.com>
2025-08-04 17:27:12 -0300

    Refactor useProfessionalIncludes hook: Introduce API_BASE_URL constants for demo and production environments, enhance data fetching logic with useCallback, and add isDemo state management for improved functionality and clarity.

--- hooks/useProfessionalIncludes.ts
+++ hooks/useProfessionalIncludes.ts
@@ -1,17 +1,21 @@
+// hooks/useProfessionalIncludes.ts
 "use client";
-import { useState, useEffect } from "react";
+
+import { useState, useEffect, useCallback } from "react";
 import Cookies from "js-cookie";
-import { ProfessionalInformation } from "@/interfaces";
 import axios from "axios";
+import { ProfessionalInformation } from "@/interfaces";
+import { API_BASE_URL } from "@/lib/constants.api";
 
 export function useProfessionalIncludes() {
   const [data, setData] = useState<ProfessionalInformation | null>(null);
   const [error, setError] = useState<Error | null>(null);
-  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isDemo, setIsDemo] = useState(false);
 
-  const fetchData = async () => {
+  const fetchData = useCallback(async () => {
     const professionalId = Cookies.get("professional-id");
-    const isDemo = Cookies.get("isDemo") === "true";
+    const isDemoCookie = Cookies.get("isDemo") === "true";
     const token = Cookies.get("session-token");
 
     if (!professionalId || !token) {
@@ -20,10 +24,9 @@ export function useProfessionalIncludes() {
       return;
     }
 
+    const baseURL = isDemoCookie ? API_BASE_URL.demo : API_BASE_URL.prod;
+
     const endpoint = `/professional/get-professional/${professionalId}`;
-    const baseURL = isDemo
-      ? "https://medical-schedule-server-demo.onrender.com/api"
-      : "https://medical-schedule-server.onrender.com/api";
 
     try {
       const response = await axios.get<ProfessionalInformation>(
@@ -34,27 +37,28 @@ export function useProfessionalIncludes() {
           },
         }
       );
-
+      setIsDemo(isDemoCookie);
       setData(response.data);
     } catch (err: any) {
-      console.error("❌ Error en la petición:", err);
+      console.error("❌ Error al obtener información del profesional:", err);
       setError(err);
     } finally {
       setIsLoading(false);
     }
-  };
+  }, []);
 
   useEffect(() => {
     fetchData();
-  }, []);
+  }, [fetchData]);
 
   return {
     data,
     appointments: data?.appointmentsIncluded ?? [],
     patients: data?.patientsIncluded ?? [],
     institutions: data?.institutionsIncluded ?? [],
-    error,
     isLoading,
+    isDemo,
+    error,
     refetch: fetchData,
   };
 }
---
+++ lib/constants.api.ts
@@ -0,0 +1,5 @@
+export const API_BASE_URL = {
+    demo: "https://medical-schedule-server-demo.onrender.com/api",
+    prod: "https://medical-schedule-server.onrender.com/api",
+  };
+  
