e088b3f532db1b90a2449eab43e4224eb32b99fc
florMignini <flormignini29@gmail.com>
2025-09-01 09:36:47 -0300

    update submit button message

--- components/SubmitButton.tsx
+++ components/SubmitButton.tsx
@@ -1,33 +1,44 @@
-import Image from 'next/image';
-import spinner from '../public/assets/icons/loader.svg'
+import Image from "next/image";
+import spinner from "../public/assets/icons/loader.svg";
 
 interface SubmitButtonI {
-    loading?: boolean;
-    className?: string;
-    children: React.ReactNode;
+  loading?: boolean;
+  className?: string;
+  children: React.ReactNode;
   disabled?: boolean;
 }
 
-const SubmitButton = ({loading, className, children, disabled}: SubmitButtonI) => {
+const SubmitButton = ({
+  loading,
+  className,
+  children,
+  disabled,
+}: SubmitButtonI) => {
   return (
-   <button
-   type='submit'
-   disabled={disabled ? disabled : loading}
-   className={`text-sm font-medium tracking-w-normal text-black transition duration-200 ease-in-out ${className}`}
-   >
-    {loading ? (
-        <div className='flex items-center gap-3'>
-            <Image
+    <button
+      type="submit"
+      disabled={disabled ? disabled : loading}
+      className={`text-sm font-medium tracking-w-normal text-black transition duration-200 ease-in-out ${className}`}
+    >
+      {loading ? (
+        <div className="flex items-center justify-start pl-3 gap-3">
+          <Image
             src={spinner}
-            alt='loader'
+            alt="loader"
             width={24}
             height={24}
-            className='animate-spin flex items-center justify-center'
-            />
+            className="animate-spin flex items-center justify-center"
+          />
+          <p className="text-sm font-medium italic tracking-w-normal text-black flex items-center justify-center">
+            {" "}
+            Aguarde un momento por favor ...{" "}
+          </p>
         </div>
-    ) : children}
-   </button>
-  )
-}
+      ) : (
+        children
+      )}
+    </button>
+  );
+};
 
-export default SubmitButton
+export default SubmitButton;
