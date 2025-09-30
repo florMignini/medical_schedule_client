d50083c198e849b40c3e61adb502816692b60287
florMignini <flormignini29@gmail.com>
2025-09-30 08:38:53 -0300

    Refactor StatsCards layout for improved styling and structure

--- app/(professional)/professional/components/StatsCards.tsx
+++ app/(professional)/professional/components/StatsCards.tsx
@@ -18,21 +18,21 @@ const iconMap: Record<string, JSX.Element> = {
 
 const StatsCards: FC<StatsCardsProps> = ({ data }) => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-3 gap-4 w-full mb-4">
+    <div className="grid justify-start sm:grid-cols-3 gap-4 w-[75%] mx-auto">
       {data.map((stat) => (
         <div
           key={stat.label}
-          className="bg-white/30 backdrop-blur-md shadow-lg rounded-2xl p-4 flex items-center justify-between border border-white/20 transition-transform hover:scale-[1.02]"
+          className="shadow-xl rounded-lg p-1 flex flex-col items-center justify-center transition-transform hover:scale-[1.02]"
         >
-          <div className="flex flex-col">
-            <span className="text-sm text-gray-800 dark:text-gray-100">
-              {stat.label}
-            </span>
+          <span className="text-sm text-gray-800 dark:text-gray-100">
+            {stat.label} totales
+          </span>
+          <div className="flex gap-2 items-center mx-auto">
             <span className="text-2xl font-bold text-gray-900 dark:text-white">
               {stat.value}
             </span>
+            <div className="flex items-center h-5 ml-1">{iconMap[stat.label]}</div>
           </div>
-          <div className="ml-4">{iconMap[stat.label]}</div>
         </div>
       ))}
     </div>
