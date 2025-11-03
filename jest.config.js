bb0539c62d5e708abf5ac5778013ce523cc4fac0
florMignini <flormignini29@gmail.com>
2025-11-03 09:53:02 -0300

    chore: update Jest configuration to improve test matching and transform settings

--- jest.config.js
+++ jest.config.js
@@ -1,29 +1,28 @@
 /** @type {import('jest').Config} */
 module.exports = {
   preset: "ts-jest",
+  rootDir: ".",
   testEnvironment: "jsdom",
-  testMatch: ["**/__tests__/**/*.[jt]s?(x)"],
 
-  // Transformar JS/TS y archivos con babel-jest
-   transform: {
+  testMatch: [
+    "**/?(*.)+(test|spec).(ts|tsx|js|jsx)",
+  ],
+
+  transform: {
     '^.+\\.(js|ts|tsx)$': ['babel-jest', { configFile: './babel.jest.js' }],
   },
 
-  // Ignorar node_modules excepto los que queremos transformar
   transformIgnorePatterns: [
     "/node_modules/(?!(node-fetch-native-with-agent|node-appwrite)/)",
   ],
 
-  // Alias y mocks de módulos
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/$1",
     "\\.(css|less|scss|sass)$": "identity-obj-proxy",
     "\\.(svg)$": "<rootDir>/__mock__/svgMock.js",
   },
 
-  // Configuración para inicializar jest
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
 
-  // Opcional: mostrar warnings de transformaciones
   verbose: true,
 };
